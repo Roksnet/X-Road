b11537c7289abf07a35e447a5779ac93f76c82c6
Riho Hiiepuu <riho.hiiepuu@roksnet.com>
2021-09-01 13:25:20 +0300

    attempt to fix checkstyle errors 3

--- src/common-util/src/test/java/ee/ria/xroad/common/certificateprofile/impl/NeomCertificateProfileInfoProviderTest.java
+++ src/common-util/src/test/java/ee/ria/xroad/common/certificateprofile/impl/NeomCertificateProfileInfoProviderTest.java
@@ -83,8 +83,9 @@ public class NeomCertificateProfileInfoProviderTest {
                         .setReadOnly(true),
                 new EnumLocalizedFieldDescriptionImpl("C", DnFieldLabelLocalizationKey.COUNTRY_CODE, "XX")
                         .setReadOnly(true),
-                new EnumLocalizedFieldDescriptionImpl("serialNumber", DnFieldLabelLocalizationKey.MEMBER_CODE, "XX/server/Foo")
-                        .setReadOnly(true),
+                new EnumLocalizedFieldDescriptionImpl("serialNumber", DnFieldLabelLocalizationKey.MEMBER_CODE, 
+                        "XX/server/Foo"
+                        ).setReadOnly(true),
                 
         };
 
