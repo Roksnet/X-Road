cc3beda8fbba2f302a50bcd7235977fad03877f3
Riho Hiiepuu <riho.hiiepuu@roksnet.com>
2021-09-06 14:57:58 +0300

    sytle error

--- src/common-util/src/main/java/ee/ria/xroad/common/certificateprofile/impl/GenericCertificateProfileInfoProvider.java
+++ src/common-util/src/main/java/ee/ria/xroad/common/certificateprofile/impl/GenericCertificateProfileInfoProvider.java
@@ -38,6 +38,7 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 
 import javax.security.auth.x500.X500Principal;
+
 import java.security.cert.X509Certificate;
 
 /**
